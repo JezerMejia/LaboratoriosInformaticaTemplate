2b01e64db7d272449579a0489cef487b6788c3c0
gabrielMerino <gabriel.merino13079@est.uca.edu.ni>
2023-04-29 17:59:11 -0700

    Feedback de Inputs implementado

--- Input Templates/main.js
+++ Input Templates/main.js
@@ -1,14 +1,13 @@
-const btnSpinnerUp = document.querySelector('.btn--spinner-up')
-const btnSpinnerDown = document.querySelector('.btn--spinner-down')
-const ddlAmPm = document.getElementById('ddlAmPm')
+
 
 function validarNumeros(input) {
     // Eliminar cualquier caracter que no sea un número
     input.value = input.value.replace(/\D/g, "");
 }
 
-function spinnerUp(inputClass){
+function spinnerUp(inputClass, selectId) {
     const input = document.querySelector(`.${inputClass}`)
+    const ddlAmPm = document.getElementById(selectId)
     let value = input.value
 
     // Eliminar cualquier caracter que no sea un número
@@ -17,42 +16,55 @@ function spinnerUp(inputClass){
     //Aumenta el valor que tiene
     value = parseInt(value) + 1
 
+    // El "icon element" tiene que tener "hour" en la clase que se pase para que esto funcione, igual para minutos.
+    if(inputClass.includes("hour")){
+        if (value == 13) {
+            value = 1    
+        }
+        if(value == 12){
+            if (ddlAmPm.selectedIndex == 0) {
+                ddlAmPm.selectedIndex = 1
+            }
+    
+            else if (ddlAmPm.selectedIndex == 1) {
+                ddlAmPm.selectedIndex = 0
+            }
+        }
+    }
+    else if(inputClass.includes("minute")){
+        if (value == 60) {
+            value = 0    
+        }
+    }
+    
+    
+
+    
+
     // Agregar un cero delante si la cantidad es menor que 10
-    if (value < 10) {
-        value = "0" + value;
-    }
-
-    if(value == 13){
-        value = 1
-        if(ddlAmPm.selectedIndex == 0){
-            ddlAmPm.selectedIndex = 1
-        }
-
-        else if(ddlAmPm.selectedIndex == 1){
-            ddlAmPm.selectedIndex = 0
-        }
-         
-    }
-
-    input.value = value
+    input.value = value < 10 ? `0${value}` : value;
 }
 
-function spinnerDown(inputClass){
+function spinnerDown(inputClass) {
     const input = document.querySelector(`.${inputClass}`)
     let value = input.value
 
     value = value.replace(/\D/g, "");
     value = parseInt(value) - 1
 
-    if (value < 10) {
-        value = "0" + value;
+    if(inputClass.includes("minute")){
+        if (value < 0) {
+            value = 0    
+        }
+    }
+    else if(value < 1) {
+        value = 1;
     }
 
-    input.value = value
+    input.value = value < 10 ? `0${value}` : value;
 }
 
-function setCategory(inputId, value){
+function setCategory(inputId, value) {
     const input = document.querySelector(`#${inputId}`)
-    console.log("Hola")
     input.value = value
 }
